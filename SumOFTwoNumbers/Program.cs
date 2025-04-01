c35e0666dc344d620435ce237f7ca8cc804834fe
nikolaajdukovic <nikolaa@examroom.ai>
2025-04-01 16:11:00 +0200

    added modified docker file

--- SumOFTwoNumbers/Program.cs
+++ SumOFTwoNumbers/Program.cs
@@ -1,30 +1,32 @@
 using Swashbuckle.AspNetCore.SwaggerUI;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(swaggerUIConfig =>
+    public static void Main(string[] args)
     {
-        swaggerUIConfig.ConfigObject.TryItOutEnabled = true;
-    });
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(swaggerUIConfig =>
+            {
+                swaggerUIConfig.ConfigObject.TryItOutEnabled = true;
+            });
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
